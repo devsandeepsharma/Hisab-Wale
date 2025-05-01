109787b740d6b48090d74f3dbcf42fed45f61b9b
Sandeep <sandipsharma2445@gmail.com>
2025-05-01 14:34:58 +0530

    react-router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+import Layout from "./components/layout/Layout";
+
 const App = () => {
-  return <h1>Hisab Wale</h1>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <h1>Home Page</h1>
+        },
+        {
+          path: "/landing",
+          element: <h1>Landing Page</h1>
+        },
+        {
+          path: "/login",
+          element: <h1>Login Page</h1>
+        },
+        {
+          path: "/signup",
+          element: <h1>Signup Page</h1>
+        },
+        {
+          path: "/transactions",
+          element: <h1>Transactions Page</h1>
+        },
+      ]
+    }
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App;
