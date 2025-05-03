bd1768242c51f1ad4bb3b9531ee8a578d1184b0c
Sandeep <sandipsharma2445@gmail.com>
2025-05-03 12:42:51 +0530

    redux configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
+import store from './store/index.js';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
